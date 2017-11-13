a84f7043621d3478a948856b59604be20e6f3027
HHHJiro <1445422321@qq.com>
2017-11-13 19:16:21 +0800

    add url loader

---
+++ webpack.maxnet.config.prod.js
@@ -0,0 +1,116 @@
+var path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: {
+    match: './src/pages/matches/dota/match_tml/index.js',
+    vender: './src/vender/zepto.min.js'
+  },
+  output: {
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    hot: true, // 告诉 dev-server 我们在使用 HMR
+    port: 3888,
+    host: '192.168.1.153',
+    proxy: {
+      '/live_stats/*': {
+        target: 'http://192.168.1.153:18080/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/live_stats/json/': '/page/'
+        }
+      }
+    },
+    contentBase: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true // css压缩
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.art$/,
+        loader: 'art-template-loader',
+        options: {
+          // art-template options (if necessary)
+          // @see https://github.com/aui/art-template
+          htmlResourceRoot: __dirname,
+          root: path.resolve(__dirname)
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: [require('babel-plugin-transform-object-rest-spread'), require('babel-plugin-transform-runtime')]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']), // 清理dist目录
+    new webpack.HotModuleReplacementPlugin(), // 启用 HMR
+    new ExtractTextPlugin('[name]/styles.[contenthash].css'),
+    new HtmlWebpackPlugin({
+      filename: 'dota_match_tml.html',
+      template: './src/pages/matches/dota/match_tml/index.art',
+      xhtml: true, // 需要符合xhtml的标准
+      chunks: ['match', 'vender'],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      warnings: false,
+      compress: {
+        join_vars: true,
+        warnings: false
+      },
+      toplevel: false,
+      ie8: false
+    })
+  ]
+}
