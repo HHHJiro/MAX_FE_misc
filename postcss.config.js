ffd71f30517c816f7a8198e7bd087d985a113b6c
HHHJiro <1445422321@qq.com>
2017-11-13 17:37:09 +0800

    delect px2vw

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,5 @@
-var pxtoviewport = require('postcss-px-to-viewport')
-var pxtoviewportConf = {
-  viewportWidth: 750,
-  viewportHeight: 1334,
-  unitPrecision: 5,
-  viewportUnit: 'vw',
-  selectorBlackList: [],
-  minPixelValue: 1,
-  mediaQuery: false
-}
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    // require('postcss-write-svg'),
-    // pxtoviewport(pxtoviewportConf)
+    require('autoprefixer')
   ]
 }
