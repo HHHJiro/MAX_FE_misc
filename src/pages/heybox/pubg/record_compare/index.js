ea72519a30844e36baa9ce38a14c398a2e9f6b72
HHHJiro <1445422321@qq.com>
2017-12-06 16:27:38 +0800

    django and ajax

--- src/pages/heybox/pubg/record_compare/index.js
+++ src/pages/heybox/pubg/record_compare/index.js
@@ -8,20 +8,33 @@ if (typeof document === 'object') {
     var nickname = window.nickname
     var season = window.season
     var bind_nickname = window.bind_nickname
-    var myInfo = {nickname: bind_nickname, season: season}
-    
-    var otherInfo = {nickname: nickname, season: season}
-    fly.all([fly.get(getRecords(myInfo)), fly.get(getRecords(otherInfo))])
-      .then(fly.spread(function (myRecordsResult, otherRecordsResult) {
-        // 两个请求都完成
-        var {data: {result: myRecordsData}} = myRecordsResult
-        var {data: {result: otherRecordsData}} = otherRecordsResult
-
-        var app = new App(myRecordsData, otherRecordsData)
-        app.initApp()
-      })).catch( e => {
-        dataGetErrorHandler(e)
+    fly.get('/game/pubg/compare/web/?heybox_id=105091&version=1.0.2&os_type=iOS&season=2017pre-6&region=as&nickname=PDD7&return_json=1')
+      .then( res => {
+        var data = res.data.result
+        var bind_nickname = data.bind_nickname
+        var nickname = data.nickname
+        var season = data.season
+        getCompare(bind_nickname, nickname, season)
       })
+    
+    var getCompare = (bind_nickname, nickname, season) => {
+      var myInfo = {nickname: bind_nickname, season: season}
+
+      var otherInfo = {nickname: nickname, season: season}
+      fly.all([fly.get(getRecords(myInfo)), fly.get(getRecords(otherInfo))])
+        .then(fly.spread(function (myRecordsResult, otherRecordsResult) {
+          // 两个请求都完成
+          var {data: {result: myRecordsData}} = myRecordsResult
+          var {data: {result: otherRecordsData}} = otherRecordsResult
+
+          var app = new App(myRecordsData, otherRecordsData)
+          app.initApp()
+        }))
+        .catch( e => {
+          dataGetErrorHandler(e)
+        })
+    }
+    // getCompare()
   }
 }
 var dataGetErrorHandler = (e) => {
