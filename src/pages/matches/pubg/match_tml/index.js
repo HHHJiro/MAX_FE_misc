a9ed929a9c9051e3b80c15cceaca2c77335a6a7e
HHHJiro <1445422321@qq.com>
2018-01-08 20:58:33 +0800

    scroll

--- src/pages/matches/pubg/match_tml/index.js
+++ src/pages/matches/pubg/match_tml/index.js
@@ -8,12 +8,12 @@ var fly = require('flyio')
 // /live_stats/json/pubg_match.json
 // http://192.168.1.153:18080/page/pubg_match.json
 var pubgDataUrl = PRODUCTION ? '/live_stats/json/pubg_match.json' : 'http://192.168.1.153:18080/page/pubg_match.json'
-var scoreRankDataUrl = PRODUCTION ? 'https://dotamax.net/static/pubg_league.json' : 'https://heybox.tt.maxjia.com/game/pubg_league/data/?league_id=1'
+var scoreRankDataUrl = PRODUCTION ? 'https://dotamax.net/static/pubg_league.json?heybox=1' : 'https://heybox.tt.maxjia.com/game/pubg_league/data/?league_id=1'
 if (typeof document === 'object') {
   window.onload = function () {
     var time = new Date().getTime()
     $.get( pubgDataUrl + '?' + time, function (res) {
-      $.get(scoreRankDataUrl + '?time=' + time, function (data) {
+      $.get(scoreRankDataUrl + '&time=' + time, function (data) {
         res.result = data.result
         var app = new App(res)
       })
