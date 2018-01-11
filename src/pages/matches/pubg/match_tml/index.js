5b4ec10fde0accedd8d8b96df50b8a74579a331c
HHHJiro <1445422321@qq.com>
2018-01-11 19:26:17 +0800

    17 team color

--- src/pages/matches/pubg/match_tml/index.js
+++ src/pages/matches/pubg/match_tml/index.js
@@ -6,7 +6,7 @@ var fly = require('flyio')
 
 // 通过matchId 判断是哪场比赛
 // /live_stats/json/pubg_match.json
-// http://192.168.1.153:18080/page/pubg_match.json
+// http://192.168.1.153:18080/page/pubg_match.json?id=1
 // https://heybox.tt.maxjia.com/game/pubg_league/data/?league_id=1
 var pubgDataUrl = PRODUCTION ? '/live_stats/json/pubg_match.json' : 'http://192.168.1.153:18080/page/pubg_match.json'
 var scoreRankDataUrl = PRODUCTION ? '//api.xiaoheihe.cn/game/pubg_league/data/?league_id=5' : 'http://192.168.1.153:18080/page/pubg_match.json?id=1'
