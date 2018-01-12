49413b1e6cb0f957f0856c4be9d4b248429fc74e
HHHJiro <1445422321@qq.com>
2018-01-12 17:09:42 +0800

    rank data error fix test

--- src/pages/matches/pubg/match_tml/index.js
+++ src/pages/matches/pubg/match_tml/index.js
@@ -7,12 +7,12 @@ var fly = require('flyio')
 // 通过matchId 判断是哪场比赛
 // /live_stats/json/pubg_match.json
 // http://192.168.1.153:18080/page/pubg_match.json
-var pubgDataUrl = PRODUCTION ? '/live_stats/json/pubg_match.json' : 'http://192.168.1.153:18080/page/pubg_match.json'
-var scoreRankDataUrl = PRODUCTION ? 'https://dotamax.net/static/pubg_league.json?heybox=1' : 'https://heybox.tt.maxjia.com/game/pubg_league/data/?league_id=1'
+var pubgDataUrl = PRODUCTION ? '/live_stats/json/pubg_match.json' : 'https://heybox.tt.maxjia.com/game/pubg_league/data/?league_id=3'
+var scoreRankDataUrl = PRODUCTION ? 'https://dotamax.net/static/pubg_league.json?heybox=1' : 'https://heybox.tt.maxjia.com/game/pubg_league/data/?league_id=3'
 if (typeof document === 'object') {
   window.onload = function () {
     var time = new Date().getTime()
-    $.get( pubgDataUrl + '?' + time, function (res) {
+    $.get( pubgDataUrl + '&' + time, function (res) {
       // $.get(scoreRankDataUrl + '&time=' + time, function (data) {
       //   res.result = data.result
         var app = new App(res)
